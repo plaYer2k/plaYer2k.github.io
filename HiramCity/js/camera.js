729cc3a5b3406cb051e4e41ac0bab48798ae5ec3
Koen Loogman <koen@loogman.de>
2019-05-03 02:52:31 +0200

    seperated camera class

---
+++ HiramCity/js/camera.js
@@ -0,0 +1,127 @@
+/**
+ * Camera class for p5.js
+ * 
+ * It allows moving and scaling of the scene. It just needs to be applied before everything else.
+ * There are two functions for converting canvas coordinates to the scene and vise versa.
+ * They can come in handy when interacting with the scene.
+ */
+class Camera {
+    constructor() {
+        // center point
+        this.center = createVector(width / 2, height / 2);
+
+        // offset
+        this.offset = createVector(0, 0);
+        this.maxOffset = null;
+        this.maxOffsetX = null;
+        this.maxOffsetY = null;
+
+        // scale
+        this.scale = 1.0;
+        this.minScale = null;
+        this.maxScale = null;
+    }
+
+    /**
+     * This scales the current scale with the given scale and keeps it within the limits.
+     * Scaling a current scale of 2.0 with 2.0 will result into a scale of 4.0 for example.
+     * 
+     * @param {double} scale 
+     */
+    zoom(scale) {
+        // scale
+        this.scale *= scale;
+
+        // limit scale
+        if (this.minScale) this.scale = Math.max(this.scale, this.minScale)
+        if (this.maxScale) this.scale = Math.min(this.scale, this.maxScale);
+    }
+
+    /**
+     * Changes the offset of the camera to move to a different section of the scene.
+     * 
+     * @param {Number} x 
+     * @param {Number} y 
+     */
+    move(x = 0, y = 0) {
+        //TODO: make it scale dependent
+        // adjust offset
+        this.offset.x += x;
+        this.offset.y += y;
+
+        // limit the offset
+        if (this.maxOffset) this.offset.limit(this.maxOffset);
+        if (this.maxOffsetX) this.offset.x = Math.max(this.offset.x, this.maxOffsetX);
+        if (this.maxOffsetY) this.offset.y = Math.max(this.offset.y, this.maxOffsetY);
+    }
+
+    /**
+     * Movement on the x-axis by a given number x.
+     * 
+     * @param {Number} x 
+     */
+    moveX(x = 0) {
+        this.move(x, 0);
+    }
+
+    /**
+     * Movement on the y-axis by a given number y.
+     * 
+     * @param {Number} y 
+     */
+    moveY(y = 0) {
+        this.move(0, y);
+    }
+
+    /**
+     * Converts a point on the canvas to the corresponding point of the scene.
+     * 
+     * @param {Number} x 
+     * @param {Number} y 
+     */
+    canvasToScene(x, y) {
+        return [
+            (x - this.center.x) / this.scale - this.offset.x,
+            (y - this.center.y) / this.scale - this.offset.y
+        ];
+    }
+
+    /**
+     * Converts a point on the scene to the corresponding point of the canvas.
+     * 
+     * @param {Number} x 
+     * @param {Number} y 
+     */
+    sceneToCanvas(x, y) {
+        return [
+            (x + this.center.x) * this.scale + this.offset.x,
+            (y + this.center.y) * this.scale + this.offset.y
+        ];
+    }
+
+    /**
+     * Applies the camera movement and scale to the canvas.
+     */
+    apply() {
+        // move to center
+        if (this.debug) {
+            push();
+            stroke('red');
+            line(0, 0, this.center.x, this.center.y);
+            pop();
+        }
+        translate(this.center.x, this.center.y);
+
+        // scale scene
+        scale(this.scale);
+
+        // move to offset
+        if (this.debug) {
+            push();
+            stroke('blue');
+            line(0, 0, this.offset.x, this.offset.y);
+            pop();
+        }
+        translate(this.offset.x, this.offset.y);
+    }
+}
--- HiramCity/js/sketch_test.js
+++ HiramCity/js/sketch_test.js
@@ -1,5 +1,3 @@
-
-
 class HiramCity {
   constructor() {
     // timestamps
@@ -7,6 +5,7 @@ class HiramCity {
     this.lastDraw = +moment();
     this.td = 0;
 
+    this.map = loadImage('./textures/TheFallOfHiramCity.png');
     /**
      * @type {Array<Icon>}
      */
@@ -23,6 +22,9 @@ class HiramCity {
     this.td = now - this.lastDraw;
     this.lastDraw = now;
 
+    // draw map
+    image(this.map, -this.map.width / 2, -this.map.height / 2);
+
     // draw icons
     let previousIcon = null;
     this.icons.forEach(icon => {
@@ -30,50 +32,6 @@ class HiramCity {
       icon.draw(this.td, this.initTime);
       previousIcon = icon;
     });
-  }
-}
-
-class Camera {
-  constructor() {
-    this.scale = 1.0;
-    this.offset = createVector(0, 0);
-    this.center = createVector(width / 2, height / 2);
-
-    this.minScale = 1 / 2;    // min 200%
-    this.maxScale = 1 / 0.2;  // max 20%
-  }
-
-  zoom(scale) {
-    // scale
-    this.scale *= scale;
-
-    // limit scale
-    this.scale = Math.min(Math.max(this.scale, this.minScale), this.maxScale);
-  }
-
-  canvasToScene(x, y) {
-    return [(x - this.center.x) / this.scale - this.offset.x, (y - this.center.y) / this.scale - this.offset.y];
-  }
-
-  sceneToCanvas(x, y) {
-    return [(x + this.center.x) * this.scale + this.offset.x, (y + this.center.y) * this.scale + this.offset.y];
-  }
-
-  apply() {
-    // move to offset
-    stroke('red');
-    line(0, 0, this.center.x, this.center.y);
-    translate(this.center.x, this.center.y);
-
-    // scale scene
-    scale(this.scale);
-    
-    // move to offset
-    stroke('blue');
-    line(0, 0, this.offset.x, this.offset.y);
-    translate(this.offset.x, this.offset.y);
-
-    stroke('black');
   }
 }
 
@@ -98,12 +56,11 @@ function setup() {
 function draw() {
   background(220, 215, 160);
 
-  //TODO: make speed scale dependent
   // add camera movement
-  if (keyIsDown(LEFT_ARROW))  camera.offset.x += cameraSpeed;
-  if (keyIsDown(RIGHT_ARROW)) camera.offset.x -= cameraSpeed;
-  if (keyIsDown(UP_ARROW))    camera.offset.y += cameraSpeed;
-  if (keyIsDown(DOWN_ARROW))  camera.offset.y -= cameraSpeed;
+  if (keyIsDown(LEFT_ARROW)) camera.moveX(+cameraSpeed);
+  if (keyIsDown(RIGHT_ARROW)) camera.moveX(-cameraSpeed);
+  if (keyIsDown(UP_ARROW)) camera.moveY(+cameraSpeed);
+  if (keyIsDown(DOWN_ARROW)) camera.moveY(-cameraSpeed);
 
   // apply camera
   camera.apply();
@@ -122,7 +79,7 @@ function windowResized() {
 
 function mouseWheel(event) {
   // scale camera (event delta is either -3 or +3)
-  camera.zoom((100 + event.delta * 2) / 100);
+  camera.zoom((100 - event.delta * 2) / 100);
 
   return false;
 }
